9513f6a561ed329c4ad2b3cbd4ef5bc0925840d4
Yasir1st <yasirnakano2nd@gmail.com>
2022-12-30 21:57:05 +0800

    Day 75

---
+++ latihanInheritance/Main.py
@@ -0,0 +1,13 @@
+from Hero import HeroIntelligent,HeroStrength
+
+yelan = HeroIntelligent('Yelan')
+raiden = HeroStrength('Raiden')
+
+yelan.show_info()
+raiden.show_info()
+
+yelan.gainExp = 200
+raiden.gainExp = 250
+
+yelan.show_info()
+raiden.show_info()
