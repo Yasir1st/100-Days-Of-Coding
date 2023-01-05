649324b55a476248cad23d7e68de3f16d22d4894
Yasir1st <yasirnakano2nd@gmail.com>
2023-01-05 21:40:23 +0800

    Day 81

---
+++ Read External File/main.py
@@ -0,0 +1,33 @@
+## Membaca file eksternal
+
+print(3*"=", " Membaca file txt ", 3*"=")
+file = open("Read External File/data.txt",mode="r")
+
+print(f"status read : {file.readable()}")
+print(f"status write : {file.writable()}")
+
+## baca seluruh file
+print(file.read())
+
+## baca per baris
+# print(file.readline(),end="") # baca baris pertama
+# print(file.readline(),end="") # baca baris kedua
+
+## baca semua baris sebagai list
+# print(file.readlines())
+
+print(f"apakah file sudah diclose : {file.closed}")
+file.close()
+print(f"apakah file sudah diclose : {file.closed}")
+
+print("")
+## salah satu teknik membuka file di python
+
+print(3*"=", " Membaca file txt dengan with", 3*"=")
+
+with open("Read External File/data.txt",mode="r") as file:
+    content = file.readline()
+    print(content,end="")
+    print(f"apakah file sudah diclose : {file.closed}")
+
+print(f"apakah file sudah diclose : {file.closed}")
