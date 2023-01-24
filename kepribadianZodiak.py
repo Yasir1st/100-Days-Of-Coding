d14b4469d7f652b961162fe3ba4b27f5acc53ebc
Yasir1st <yasirnakano2nd@gmail.com>
2023-01-24 21:48:32 +0800

    Last Day

---
+++ kepribadianZodiak.py
@@ -0,0 +1,231 @@
+import os
+import datetime
+os.system("cls")
+
+def line():
+    print("="*100)
+
+def aquarius():
+    print("Zodiak : Aquarius")
+    print('''
+        Kekuatan karakter zodiak Aquarius adalah progresif, original dan mandiri.
+    Kelemahan dari karakter zodiak ini kurang bisa mengekspresikan perasaan 
+    ekspresi emosional. Kelahiran Aquarius adalah pribadi yang pemalu dan pendiam,
+    tetapi di sisi lain mereka bisa menjadi sangat eksentrik dan energik.Mereka 
+    mampu melihat tanpa prasangka, pada kedua sisi, membuatnya menjadi pribadi yang 
+    mudah menyelesaikan masalah.
+        Aquarius adalah pemikir yang suka membantu orang. Mereka merasa nyaman dalam 
+    kelompok atau komunitas, sehingga mereka terus-menerus berusaha dikelilingi oleh 
+    orang lain. Meskipun mereka dapat dengan mudah beradaptasi dengan energi yang 
+    mengelilinginya, kelahiran Aquarius memiliki kebutuhan yang besar untuk menyendiri 
+    dan menjauh dari segalanya, untuk memulihkan kekuatan. Orang yang lahir di bawah 
+    tanda Aquarius memandang dunia sebagai tempat yang penuh dengan kemungkinan
+        Jika seseorang menginginkan hubungan jangka panjang dengan Aquarius, pastikan 
+    untuk menghargai integritas dan kejujuran. Dalam hubungan, mereka melihat pasangan 
+    mereka secara sederajat dan memberi kebebasan yang dibutuhkan.
+    ''')
+
+def pisces():
+    print("Zodiak : Pisces")
+    print('''
+        Kekuatan karakter zodiak Pisces adalah berbelas kasih dan intuitif. Kelemahan 
+    dari karakter zodiak ini keinginan untuk keluar dari kenyataan dan mengasihani diri 
+    sendiri. Pisces sangat ramah dan biasanya ditemani oleh berbagai jenis orang. Pisces 
+    tidak mementingkan diri sendiri, mereka selalu bersedia membantu orang lain, tanpa 
+    berharap mendapatkan apa pun kembali. Mereka intuitif, peduli dan setia.
+        Pisces sangat terhubung dengan musik dan berbagai jenis seni. Mereka adalah 
+    golongan romantis sejati. Pisces adalah pecinta yang bersemangat yang mendambakan 
+    koneksi mendalam dengan pasangannya. Dalam cinta dan hubungan, mereka sangat setia dan 
+    sangat membabi buta. Pisces adalah tanda Air dan dengan demikian karakter zodiak ini 
+    ditandai oleh empati dan kapasitas emosional yang diungkapkan.
+    ''')
+
+def aries():
+    print("Zodiak : Aries")
+    print('''
+        Kekuatan karakter zodiak Aries adalah berani dan bersemangat. Kelemahan dari 
+    karakter zodiak ini tidak sabaran dan agresif. Aries adalah tanda pertama Zodiak, mereka 
+    biasanya yang pertama mengambil tindakan dalam situasi. Karena dikuasai oleh planet 
+    Mars dan elemen api, pemilik karakter zodiak Aries dianggap sebagai salah satu tanda 
+    Zodiak paling aktif. Mereka memiliki energi awet muda berapapun usianya. Mereka 
+    menunjukkan hasrat untuk hidup yang tak tertandingi oleh tanda-tanda lain.
+        Karakter zodiak Aries selalu mencari dinamika, kecepatan, kompetisi, dan selalu 
+    menjadiyang pertama dalam segala hal -mulai dari pekerjaan hingga hubungan sosial. 
+    Sudah menjadi sifat mereka untuk mengambil tindakan, meski terkadang belum dipikirkan 
+    dengan baik.
+        Jatuh cinta menurut Aries ditandai dengan api yang bersemangat. Mereka menghujani 
+    orang yang mereka cintai dengan penuh kasih sayang, terkadang bahkan sampai level ekstrem. 
+    Aries sangat menginginkan petualangan dan kegembiraan dalam hubungan, sehingga mereka 
+    membutuhkan pasangan yang dapat mengimbangi energi tinggi mereka.
+    ''')
+
+def taurus():
+    print("Zodiak : Taurus")
+    print('''
+        Kekuatan karakter zodiak Taurus adalah bisa diandalkan dan setia. Kelemahan karakter 
+    zodiak ini adalah keras kepala dan posesif. Karena Taurus adalah tanda Bumi, mereka dapat 
+    melihat kehidupan dari perspektif yang realistis dan membumi. Walaupun kekeraskepalaan 
+    adalah salah satu kelemahan mereka, hal itu juga dapat dilihat sebagai atribut positif 
+    karena mereka dapat bertahan pada rencana yang sama selama bertahun-tahun atau sampai 
+    selesai. Ini menjadikan Taurus teman yang sangat baik, teman jangka panjang yang hebat, 
+    dan mitra.
+        Dipimpin oleh Venus (planet cinta), Taurus memiliki rasa cinta akan keindahan, 
+    ketertarikan, kepuasan, dan rasa terima kasih. Dalam cinta, pasangan mereka harus bersabar 
+    karena Taurus menginginkan lingkungan yang aman sebelum sepenuhnya intim dengan seseorang. 
+    Mereka sangat sensual, berkembang saat disentuh. Untuk hubungan jangka panjang, mereka 
+    biasanya mencari mitra yang memiliki lingkungan sosial yang sama dengan mereka.
+    ''')
+
+def gemini():
+    print("Zodiak : Gemini")
+    print('''
+        Kekuatan karakter zodiak Gemini adalah pribadi yang penyayang dan penuh rasa penasaran. 
+    Kelemahan karakter zodiak ini adalah gampang gelisah, tidak konsisten, dan ragu-ragu. Gemini 
+    bersifat sosial, komunikatif, dan siap untuk bersenang-senang. Karena kepribadian ganda mereka, 
+    kadang-kadang mereka bisa serius dan sangat ingin tahu sehingga berusaha mempelajari segala 
+    sesuatu tentang dunia. Orang yang terlahir dalam zodiak ini merasa seolah-olah separuh dirinya 
+    hilang, sehingga mereka terus mencari orang baru untuk diajak berbicara.
+        Untuk urusan cinta, karakter zodiak Gemini percaya bahwa komunikasi sama pentingnya dengan 
+    sentuhan fisik dalam suatu hubungan. Selalu siap untuk menggoda, Gemini bisa mendapatkan banyak
+    jenis kekasih yang berbeda sampai menemukan yang diyakini dapat mengimbangi kepribadian mereka. 
+    Tantangan terbesar mereka dalam suatu hubungan adalah menemukan hubungan emosional yang kuat 
+    dengan orang lain.
+    ''')
+
+def cancer():
+    print("Zodiak : Cancer")
+    print('''
+        Kekuatan karakter zodiak Cancer adalah sangat imajinatif, setia, dan simpatik. Kelemahan 
+    karakter zodiak ini adalah moody dan manipulatif. Cancer sangat emosional dan sensitif, dan 
+    senang terhubung dengan hal-hal yang berkaitan dengan rumah mereka. Cancer sangat peduli pada 
+    keluarga dan dekat dengan teman-temannya. Cancer adalah tanda air sehingga karena emosi di dalam 
+    dirinya, mereka memiliki kesulitan menyatu dengan dunia 
+    di sekitarnya.
+        Cancer dalam hal cinta dan hubungan sangat menghargai perasaan di atas hal lain. Mereka suka 
+    memilih mitra yang dapat mereka ajak komunikasi dalam diam. Rutinitas sehari-hari bersama pasangan
+    juga penting bagi pemilik zodiak Cancer karena dapat menjadi sarana pertumbuhan hubungan mereka.
+    ''')
+
+def leo ():
+    print("Zodiak : Leo")
+    print('''
+        Kekuatan karakter zodiak Leo adalah kreatif dan berhati hangat. Kelemahan karakter zodiak ini 
+    adalah arogan dan egois. Anda akan mendengar singa mengaum. Ini karena Leo adalah pemimpin alami. 
+    Mereka percaya diri dan kreatif, dan akan dapat berhasil dalam bidang apa pun yang dipilih sebagai 
+    komitmen dalam hidupnya. Mereka sering memiliki banyak teman karena kepribadiannya yang murah hati, 
+    setia, dan ramah. Di sisi lain, Leo sedang dalam pencarian kesadaran dan pertumbuhan diri, yang 
+    mungkin dianggap egois oleh orang lain.
+        Dalam urusan percintaan, Leo cenderung murah hati dan bergairah dengan pasangannya. Leo 
+    membutuhkan pasangan yang sadar diri dan memiliki tingkat intelektual yang sederajat. Mereka akan 
+    mengambil peran pemimpin dalam hubungan apa pun. Ini karena Leo sangat percaya diri dan menarik, 
+    sehingga mampu menyatukan berbagai kelompok orang dan memimpin mereka dengan satu tujuan bersama. 
+    Selera humor Leo yang sehat membuat kolaborasi dengan orang lain menjadi lebih mudah.
+    ''')
+
+def virgo():
+    print("Zodiak : Virgo")
+    print('''
+        Kekuatan karakter zodiak Virgo adalah setia dan pekerja keras. Kelemahan karakter zodiak adalah 
+    suka khawatir dan mengkritik diri sendiri serta orang lain. Virgo dianggap sebagai tanda zodiak 
+    yang paling berhati-hati. Dalam hidupnya, Virgo selalu merencanakan banyak hal semaksimal mungkin. 
+    Virgo sering disalahpahami karena ketidakmampuan mereka untuk menerima perasaan mereka sebagai 
+    suatu hal yang valid.
+        Karena Virgo berada di bawah planet Merkurius, Virgo dapat memiliki indera bicara dan penulisan 
+    yang berkembang dengan baik. Dalam masalah percintaan, Virgo jarang memberikan pernyataan cinta 
+    terarah. Namun, Virgo akan menunjukkan cinta pada pasangannya melalui keintiman. Virgo lebih suka 
+    hubungan yang stabil daripada teman kencan biasa.
+    ''')
+
+def libra():
+    print("Zodiak : Libra")
+    print('''
+        Kekuatan karakter zodiak Libra adalah bersikap kooperatif dan berpikiran adil. Kelemahan 
+    karakter zodiak adalah suka ragu-ragu dan cenderung menghindari konfrontasi. Libra terpesona oleh 
+    keseimbangan. Mereka juga menghindari konflik seperti wabah sebisa mungkin, untuk mempertahankan 
+    suasana dan situasi damai. Karena Libra adalah pertanda Udara, hal ini membuat Libra menjadi seorang 
+    pencari yang konstan dalam hal pengetahuan. Kehidupan mereka perlu diperkaya dengan musik dan seni.
+        Dalam hubungan romantis, Libra ingin menemukan keseimbangan dan harmoni dengan pasangannya. 
+    Libra mencari hubungan emosional yang mendalam dengan pasangannya. Libra juga mendukung jalur 
+    tradisional untuk hubungan dan akan melihat ke arah pernikahan. Mereka menginginkan pasangan 
+    seumur hidup.
+    ''')
+
+def scorpio():
+    print("Zodiak : Scorpio")
+    print('''
+        Kekuatan karakter zodiak Scorpio adalah berani dan banyak akal. Kelemahan karakter zodiak 
+    ini adalah gampang cemburu dan tertutup. Scorpio adalah orang-orang yang bersemangat, berani, 
+    dan tegas. Karena itu, mereka menjalani kehidupan dengan keuletan yang kuat.
+        Scorpio sangat menghargai kejujuran dan akan menyimpan rapat-rapat rahasia Anda. Mereka 
+    dikenal karena perilakunya yang tenang dan memiliki aura misterius. Scorpio juga sangat membenci 
+    ketidakjujuran.
+        Scorpio adalah tanda paling sensual dari keseluruhan zodiak. Karena sifatnya yang penuh 
+    gairah, keintiman sangat penting bagi mereka. Mereka memulai hubungan dengan hati-hati sehingga 
+    kepercayaan dapat dibangun.
+    ''')
+
+def sagitarius():
+    print("Zodiak : Sagitarius")
+    print('''
+        Kekuatan karakter zodiak Sagitarius adalah idealis dan memiliki selera humor yang tinggi. 
+    Kelemahan karakter zodiak ini adalah tidak tersaring dan sangat tidak sabaran. Orang-orang yang 
+    dilahirkan di bawah tanda ini memiliki kepribadian yang ekstrovert, antusias, dan dinamis.
+        Sagitarius sangat menghargai kebebasan, karena dengan begitu mereka dapat melakukan perjalanan 
+    dan menjelajahi dunia dan budaya lain. Mereka sering bisa tidak sabar ketika berhadapan dengan 
+    hal-hal yang mereka inginkan dan harus belajar mengekspresikan diri dengan cara yang menghormati 
+    orang lain juga.
+        Dalam hubungan percintaan, Sagitarius menginginkan pasangan yang dapat mereka ajak bersenang-
+    senang karena sifat main-main mereka. Karena kecintaan mereka terhadap perbedaan, Sagitarius 
+    menemukan dirinya sering terhubung dengan banyak orang yang berbeda. Tetapi ketika sedang jatuh 
+    cinta, mereka akan sangat setia kepada pasangannya.
+    ''')
+
+def capricorn():
+    print("Zodiak : Capricorn")
+    print('''
+        Kekuatan karakter zodiak Capricorn adalah bertanggung jawab, disiplin dan memiliki kendali diri. 
+    Kelemahan karakter zodiak ini adalah senang menjadi orang yang tahu segalanya dan sulit memaafkan. 
+    Orang yang lahir di bawah tanda ini, memiliki rasa kemandirian batin yang membantu mendorong maju 
+    guna mencapai tujuan pribadi dan profesional mereka. Mereka ahli membuat rencana dan menindaklanjutinya. 
+    Capricorn adalah penguasa kontrol diri.
+        Tidak mudah bagi seseorang untuk meruntuhkan tembok Capricorn. Tetapi begitu ada yang bisa 
+    melakukannya, mereka akan memiliki komitmen seumur hidup dalam hubungan tersebut. Berbicara tentang 
+    emosi tidak akan mudah dengan orang yang memiliki zodiak Capricorn. Tetapi begitu dirinya terbuka, 
+    Capricorn dan pasangannya dapat memiliki hubungan yang tumbuh dengan kekal.
+    ''')
+
+
+line()
+print("Program Zodiak")
+line()
+
+tanggal = int(input("Tanggal Lahir \t: "))
+bulan = int(input("Bulan Lahir \t: "))
+tahun = int(input("Tahun Lahir \t: "))
+
+line()
+
+if((tanggal >= 20 and bulan == 1)or(tanggal <= 18 and bulan == 2)):
+    aquarius()
+elif((tanggal >= 19 and bulan == 2)or(tanggal <= 20 and bulan == 3)):
+    pisces()
+elif((tanggal >= 21 and bulan == 3)or(tanggal <= 19 and bulan == 4)):
+    aries()
+elif((tanggal >= 20 and bulan == 4)or(tanggal <= 20 and bulan == 5)):
+    taurus()
+elif((tanggal >= 21 and bulan == 5)or(tanggal <= 20 and bulan == 6)):
+    gemini()
+elif((tanggal >= 21 and bulan == 6)or(tanggal <= 22 and bulan == 7)):
+    cancer()
+elif((tanggal >= 23 and bulan == 7)or(tanggal <= 22 and bulan == 8)):
+    leo()
+elif((tanggal >= 22 and bulan == 8)or(tanggal <= 22 and bulan == 9)):
+    virgo()
+elif((tanggal >= 23 and bulan == 9)or(tanggal <= 22 and bulan == 10)):
+    libra()
+elif((tanggal >= 23 and bulan == 10)or(tanggal <= 21 and bulan == 11)):
+    scorpio()
+elif((tanggal >= 21 and bulan == 11)or(tanggal <= 22 and bulan == 12)):
+    sagitarius()
+elif((tanggal >= 21 and bulan == 12)or(tanggal <= 19 and bulan == 1)):
+    capricorn()
